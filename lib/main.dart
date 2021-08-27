287bfd198008f8cb004e28fda4f1602ff0734928
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-27 07:48:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.green,
+          body: Container(
+            margin: EdgeInsets.all(10),
+            child: ListView(
+              children: [
+                Card(
+                  child: Row(
+                    children: [
+                      Icon(Icons.account_box),
+                      Text('Account Box'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
